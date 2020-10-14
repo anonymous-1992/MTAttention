71b7db45d666f3973efc129ce361dd544166bf13
sepidekoohfar <sepide1.koohfar@gmail.com>
2020-10-14 13:02:45 -0400

    Fixing the input and output shapes

--- Transformer.py
+++ Transformer.py
@@ -20,6 +20,7 @@ np.random.seed(SEED)
 
 
 class PositionwiseFeedForward(nn.Module):
+
     "Implements FFN equation."
     def __init__(self, d_model, d_ff, dropout=0.1):
         super(PositionwiseFeedForward, self).__init__()
@@ -32,6 +33,7 @@ class PositionwiseFeedForward(nn.Module):
 
 
 class EncoderLayer(nn.Module):
+
     def __init__(self, d_model, d_forward, n_head, dropout=0.1):
         super(EncoderLayer, self).__init__()
         self.self_attn = nn.MultiheadAttention(d_model, n_head, dropout=dropout)
@@ -50,6 +52,7 @@ class EncoderLayer(nn.Module):
 
 
 class DecoderLayer(nn.Module):
+
     def __init__(self, d_model, d_forward, n_head, dropout=0.1):
         super(DecoderLayer, self).__init__()
         self.slf_attn = nn.MultiheadAttention(d_model, n_head, dropout=dropout)
@@ -74,6 +77,7 @@ class DecoderLayer(nn.Module):
 
 
 class Encoder(nn.Module):
+
     def __init__(self, encoder_layer, num_layers):
         super(Encoder, self).__init__()
         self.layer_stacks = nn.ModuleList([
@@ -90,6 +94,7 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
+
     def __init__(self, decoder_layer, num_layers):
 
         super(Decoder, self).__init__()
@@ -108,6 +113,7 @@ class Decoder(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
+
     "Implement the PE function."
     def __init__(self, d_model, dropout, max_len=5000):
         super(PositionalEncoding, self).__init__()
@@ -130,18 +136,24 @@ class PositionalEncoding(nn.Module):
 
 
 class AR(nn.Module):
+
     def __init__(self, window, horizon):
+
         super(AR, self).__init__()
         self.linear = nn.Linear(window, horizon)
 
     def forward(self, x):
+
+        x = x.permute(0, 2, 1)
         x = self.linear(x)
+        x = x.permute(0, 2, 1)
         return x
 
 
 class Trns_Model(nn.Module):
+
     def __init__(self, d_model, n_head, num_enc_layers, num_dec_layers
-                 , d_forward, input_size, output_size, encode_length, dropout, add_ar):
+                 , d_forward, window, horizon, input_size, output_size, encode_length, dropout, add_ar):
         super(Trns_Model, self).__init__()
         self.d_model = d_model
         self.input_size = input_size
@@ -151,7 +163,7 @@ class Trns_Model(nn.Module):
         self.position_encoding = PositionalEncoding(d_model, dropout)
         self.enc_input_embed = nn.Linear(self.input_size, self.d_model)
         self.dec_input_embed = nn.Linear(self.input_size, self.d_model)
-        self.ar = AR(input_size, output_size)
+        self.ar = AR(window, horizon)
         self.linear_out = nn.Linear(d_model, output_size)
         enc_layer = EncoderLayer(d_model, d_forward, n_head, dropout)
         self.encoder = Encoder(enc_layer, num_enc_layers)
@@ -160,8 +172,8 @@ class Trns_Model(nn.Module):
 
     def forward(self, src):
 
-        enc_input = src[:, :, :]
-        dec_input = src[:, :, :]
+        enc_input = src[:, :self.encode_length, :].permute(1, 0, 2)
+        dec_input = src[:, self.encode_length:, :].permute(1, 0, 2)
 
         # shift outputs to the right by 1
         dec_input = torch.roll(dec_input, shifts=(0, 0, 1), dims=(0, 1, 2))
@@ -175,7 +187,7 @@ class Trns_Model(nn.Module):
         enc_output = self.encoder(enc_input)
         dec_output = self.decoder(dec_input, enc_output)
 
-        output = dec_output
+        output = dec_output.permute(1, 0, 2)
         decoder_output = self.linear_out(output)
 
         if self.add_ar:
@@ -192,8 +204,10 @@ def get_configs():
     parser = argparse.ArgumentParser(description='pytocrh time series forecasting Transformers')
     parser.add_argument('--data_dir', type=str, default="../data/split_ds/")
     parser.add_argument('--site', type=str, default="BDCs_1")
-    parser.add_argument('--input_size', type=int, default=5, help='window')
-    parser.add_argument('--output_size', type=int, default=1, help='horizon')
+    parser.add_argument('--input_size', type=int, default=1, help='n_features')
+    parser.add_argument('--output_size', type=int, default=1, help='target shape')
+    parser.add_argument('--window', type=int, default=128, help='window')
+    parser.add_argument('--horizon', type=int, default=28, help='horizon')
     parser.add_argument('--d_model', type=int, default=32)
     parser.add_argument('--n_head', type=int, default=8)
     parser.add_argument('--act_type', type=str, default='relu')
@@ -201,7 +215,7 @@ def get_configs():
     parser.add_argument('--d_forward', type=int, default=4)
     parser.add_argument('--num_encoder_layers', type=int, default=3)
     parser.add_argument('--num_decoder_layers', type=int, default=3)
-    parser.add_argument('--ephocs', type=int, default=100)
+    parser.add_argument('--ephocs', type=int, default=50)
     parser.add_argument('--time_steps', type=int, default=128)
     parser.add_argument('--encode_length', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=64)
@@ -223,7 +237,8 @@ def data_loader(params, set_type):
     data = data[:-2 * params.prediction_length] if set_type == 'train' else \
         data[-2 * params.prediction_length:- params.prediction_length] if set_type == 'valid' else \
         data[-params.prediction_length:]
-    dataset = trans_dataloader.transDataset(params.max_samples, params.input_size, params.output_size, params.time_steps,
+    dataset = trans_dataloader.transDataset(params.max_samples, params.window, params.horizon,
+              params.input_size, params.output_size, params.time_steps,
                            params.encode_length, data)
 
     loader = DataLoader(
@@ -235,15 +250,18 @@ def data_loader(params, set_type):
 
 
 def create_plot(labels, predictions, rmse, pred_len, fname):
+
     fig, ax = plt.subplots()
     ax.plot(np.arange(0, pred_len), labels.detach().numpy(), color='lime', label="Measurements")
-    ax.plot(np.arange(0, pred_len), predictions.detach().numpy(), color='darkorange', label="Predictions rmse: %.2f" % rmse)
+    ax.plot(np.arange(0, pred_len), predictions.detach().numpy(), color='darkorange',
+            label="Predictions rmse: %.2f" % rmse)
     plt.legend()
     plt.tight_layout()
     plt.savefig("%s.svg" % fname)
 
 
-def evaluate(params, model, eval_loader, fname):
+def evaluate(params, model, eval_loader):
+
     model.eval()
     total_samples = 0
     total_loss = 0
@@ -265,7 +283,6 @@ def evaluate(params, model, eval_loader, fname):
     predict = predict.reshape((len(predict) * params.time_steps * params.output_size, ))
     labels = labels.reshape((len(labels) * params.time_steps * params.output_size, ))
     rmse = math.sqrt(total_loss / total_samples)
-    create_plot(labels, predict, rmse, 128, fname)
 
     y_diff = predict - labels
     y_mean = torch.mean(labels)
@@ -282,7 +299,7 @@ def evaluate(params, model, eval_loader, fname):
     corr_inter = corr_num / corr_denom
     corr = torch.sum(corr_inter)
 
-    return rmse, rrse, corr
+    return rmse, rrse, corr, predict, labels
 
 
 def train(params, model, train_loader, criterion):
@@ -321,31 +338,34 @@ def run_models(params, model, fname):
     for i in range(params.ephocs):
         model.train()
         loss = train(params, model, train_iter, criterion)
-        #print('train loss: {:5.2f}'.format(loss))
+        print('train loss: {:5.2f}'.format(loss))
 
-        mse, rrse, corr = evaluate(params, model, valid_iter, fname)
-        #print('validation mse: {:5.2f}, validation rrse: {:5.2f}, validation corr: {:5.2f}'.format(mse, rrse, corr))
+        rmse, rrse, corr, _, _ = evaluate(params, model, valid_iter)
+        print('validation rmse: {:5.2f}, validation rrse: {:5.2f}, validation corr: {:5.2f}'.
+              format(rmse, rrse, corr))
 
-        if mse < best_val:
+        if rmse < best_val:
             with open(params.save, 'wb') as f:
                 torch.save(model, f)
-            best_val = mse
+            best_val = rmse
+
     with open(params.save, 'rb') as f:
         model = torch.load(f)
-    mse, rrse, corr = evaluate(params, model, test_iter, fname)
-    print('test mse: {:5.2f}, test rrse: {:5.2f}, test corr: {:5.2f}'.format(mse, rrse, corr))
+    rmse, rrse, corr, predict, labels = evaluate(params, model, test_iter)
+    print('test mse: {:5.2f}, test rrse: {:5.2f}, test corr: {:5.2f}'.format(rmse, rrse, corr))
+    create_plot(labels, predict, rmse, params.time_steps * params.output_size, fname)
 
 
 def main():
 
     params = get_configs()
     trans_model = Trns_Model(params.d_model, params.n_head, params.num_encoder_layers,
-                  params.num_decoder_layers, params.d_forward, params.input_size,
+                  params.num_decoder_layers, params.d_forward, params.window, params.horizon, params.input_size,
                   params.output_size, params.encode_length, params.dropout_rate, params.add_ar)
 
     run_models(params, trans_model, params.trns_fname)
 
-    ar_model = AR(params.input_size, params.output_size)
+    ar_model = AR(params.window, params.horizon)
 
     run_models(params, ar_model, params.ar_fname)
 
--- util/trans_dataloader.py
+++ util/trans_dataloader.py
@@ -10,27 +10,38 @@ class transDataset(Dataset):
     returns [samples, labels]
     '''
 
-    def __init__(self, max_samples, input_size, output_size, time_steps, num_encoder_steps, data):
+    def __init__(self, max_samples, window, horizon, input_size, output_size, time_steps, num_encoder_steps, data):
 
+        self.window = window
+        self.horizon = horizon
         self.input_size = input_size
         self.output_size = output_size
         self.time_steps = time_steps
         self.max_samples = max_samples
-        self.data = data
+        self.data = data.reshape(data.shape[0], 1)
         self.num_encoder_steps = num_encoder_steps
 
         len_data = len(data)
-        X = torch.zeros((len_data, self.input_size))
+        self.inputs = torch.zeros((len_data, self.window, self.input_size))
+        self.outputs = torch.zeros((len_data, self.horizon, self.output_size))
+
+        for i in range(len_data):
+            start = i
+            end = i + self.input_size
+            if end+self.output_size <= len_data:
+                self.inputs[i, :, :] = torch.from_numpy(self.data[start:end, :])
+                self.outputs[i, :, :] = torch.from_numpy(self.data[end:end + self.output_size, :])
+        '''X = torch.zeros((len_data, self.input_size))
         Y = torch.zeros((len_data, self.output_size))
 
         for i in range(len_data):
             start = i
             end = start + self.input_size
-            if end < len_data:
+            if end + self.output_size <= len_data:
                 X[i, :] = torch.from_numpy(data[start:end])
-                Y[i, :] = torch.from_numpy(data[end:end + self.output_size])
+                Y[i, :] = torch.from_numpy(data[end:end + self.output_size])'''
 
-        num_samples = int(len_data / self.time_steps)
+        '''num_samples = int(len_data / self.time_steps)
 
         self.inputs = torch.zeros((num_samples, self.time_steps, self.input_size))
         self.outputs = torch.zeros((num_samples, self.time_steps, self.output_size))
@@ -40,7 +51,7 @@ class transDataset(Dataset):
             end = start + self.time_steps
             if len(X[start:end, :]) == self.time_steps:
                 self.inputs[i, :, :] = X[start:end, :]
-                self.outputs[i, :, :] = Y[start:end, :]
+                self.outputs[i, :, :] = Y[start:end, :]'''
 
     def __getitem__(self, index):
         s = {
